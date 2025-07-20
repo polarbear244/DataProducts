42c942ef516ff1b1bfaac70a2bc2402438242f0d
polarbear244 <sarah_koesters@yahoo.de>
2025-07-20 16:54:21 +0100

    Update ui.r
    changes for how to guidance

--- ui.r
+++ ui.r
@@ -5,7 +5,16 @@ shinyUI(fluidPage(
 
   sidebarLayout(
     sidebarPanel(
-      helpText("Enter your luggage dimensions in inches."),
+      h4("Instructions 📘"),
+      helpText("This app converts your luggage dimensions from inches to centimeters."),
+      p("To use the app:"),
+      tags$ul(
+        tags$li("Enter your luggage's Length, Width, and Depth in inches."),
+        tags$li("Click the 'Convert to cm' button."),
+        tags$li("The converted dimensions and total size will appear on the right.")
+      ),
+      br(),
+      #helpText("Enter your luggage dimensions in inches."),
       numericInput("length", "Length (in):", value = NULL),
       numericInput("width", "Width (in):", value = NULL),
       numericInput("depth", "Depth (in):", value = NULL),
